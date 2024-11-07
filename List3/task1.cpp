130318638c888fe08f3f8a6d0c5de7615f18f8e9
kapiura <swinxten@gmail.com>
2024-11-08 00:00:31 +0100

    Update List 3 Add nie wiem jaki task1 pomocy prosze

---
+++ List3/task1.cpp
@@ -0,0 +1,209 @@
+
+// Task 3 - namespace/class template - cpplab::vector
+#include <concepts>
+#include <cstddef>
+#include <iostream>
+#include <stdexcept>
+#include <type_traits>
+#include <vector>
+
+namespace cpplab
+{
+	template <typename T> class vector
+	{
+	  public:
+		vector () = default;
+		vector (std::initializer_list<T> list)
+		{
+			for (const T &el: list)
+			{
+				push_back (el);
+			}
+		}
+		~vector ()
+		{
+			delete[] _tab;
+			value_type *_tab = nullptr;
+			size_t _size = 0;
+			size_t _capacity = 0;
+		};
+
+		const size_t size () const
+		{
+			return _size;
+		}
+		const size_t capacity () const
+		{
+			return _capacity;
+		}
+
+		vector &operator= (const vector &rhs)
+		{
+			if (this == &rhs)
+				return *this;
+
+			delete[] _tab;
+			_size = rhs.size ();
+			_capacity = rhs.capacity ();
+			_tab = new value_type[ _capacity ];
+
+			for (size_t i = 0; i < _size; ++i)
+			{
+				_tab[ i ] = rhs._tab[ i ];
+			}
+			return *this;
+		}
+
+		void push_back (const T &newElement)
+		{
+			if (size () == 0)
+			{
+				resize (1);
+			}
+			else if (size () >= capacity ())
+			{
+				resize (size () + 1);
+			}
+			else if (size () < capacity ())
+			{
+				_size++;
+			}
+			// _size++;
+			_tab[ size () - 1 ] = newElement;
+		}
+
+		void pop_back ()
+		{
+			if (size () > 0)
+			{
+				_size--;
+			}
+			else if (size () == 0)
+			{
+				throw std::out_of_range ("Cannot pop_back from an empty vector\n");
+			}
+		}
+
+		void resize (size_t newSize)
+		{
+			if (size () < newSize)
+			{
+				reserve (newSize * 2);
+			}
+			_size = newSize;
+		}
+
+		T &operator[] (const int &index)
+		{
+			if (index >= _size || index < 0)
+			{
+				throw std::out_of_range ("Index out of range\n");
+			}
+			return _tab[ index ];
+		}
+
+		T operator[] (const int &index) const
+		{
+			if (index >= _size || index < 0)
+			{
+				throw std::out_of_range ("Index out of range\n");
+			}
+			return _tab[ index ];
+		}
+
+		friend std::ostream &operator<< (std::ostream &os, const vector &vec)
+		{
+			os << "[";
+			for (size_t i = 0; i < vec.size (); i++)
+			{
+				if (i == vec.size () - 1)
+					os << vec[ i ];
+				else
+					os << vec[ i ] << ", ";
+			}
+			os << "]\n";
+			return os;
+		}
+
+		void printVectorInfo ()
+		{
+			std::cout << "SIZE:" << size () << "\n"; // 0
+			std::cout << "CAPACITY:" << capacity () << "\n";
+			std::cout << *this;
+			std::cout << "--------------------------------\n";
+		}
+
+	  private:
+		typedef T value_type;
+		value_type *_tab = nullptr;
+		size_t _size = 0;
+		size_t _capacity = 0;
+
+		void reserve (size_t newCapacity)
+		{
+			value_type *temp = new value_type[ newCapacity ];
+			for (size_t i = 0; i < size (); i++)
+			{
+				temp[ i ] = _tab[ i ];
+			}
+			_capacity = newCapacity;
+			delete[] _tab;
+			_tab = temp;
+		}
+	};
+} // namespace cpplab
+
+template <typename T>
+concept IsArithmetic = requires (T a) { std::is_arithmetic_v<T>; };
+
+template <typename T>
+concept Indexable = requires (T a) { a[ 0 ]; };
+
+template <typename T>
+concept VectorType = IsArithmetic<T> && Indexable<T>;
+
+template <VectorType V1, VectorType V2> auto operator* (const V1 &vec1, const V2 &vec2)
+{
+	using ResultType = decltype (vec1[ 0 ] * vec2[ 0 ]);
+
+	ResultType result = ResultType{};
+	for (size_t i = 0; i < vec1.size (); i++)
+	{
+		result += vec1[ i ] * vec2[ i ];
+	}
+	return result;
+}
+
+int main ()
+{
+	cpplab::vector<int> v1{1, 2, 3, 4};
+	std::vector<int> v2{1, 2, 3, 4};
+	int res1 = v1 * v2;
+	int res2 = v2 * v1;
+	std::cout << res1 << "\n";
+	std::cout << res2 << "\n";
+
+	cpplab::vector<float> v3{1.0, 2.0, 3.0, 4.0};
+	std::vector<float> v4{1.0, 2.0, 3.0, 4.0};
+	float res3 = v3 * v4;
+	float res4 = v4 * v3;
+	std::cout << res3 << "\n";
+	std::cout << res4 << "\n";
+
+	cpplab::vector<char> v5{'a', 'b', 'c', 'd'};
+	std::vector<char> v6{'w', 'x', 'y', 'z'};
+	std::string res5 = v5 * v6;
+	std::string res6 = v6 * v5;
+	std::cout << res5 << "\n";
+	std::cout << res6 << "\n";
+
+	cpplab::vector<std::string> v7{"Hello", "world"};
+	std::vector<std::string> v8{"essa"
+								"skibidi"};
+	std::string res7 = v7 * v8;
+	std::string res8 = v8 * v7;
+	std::cout << res7 << "\n";
+	std::cout << res8 << "\n";
+
+	return 0;
+}
--- List3/task3.cpp
+++ List3/task3.cpp
@@ -3,91 +3,95 @@
 #include <memory>
 
 // Task 3 - single linked lsit
-template <typename T>
-class Node
+template <typename T> class Node
 {
-public:
-  Node (T val) : value (val), next (nullptr) {}
-  T value;
-  std::unique_ptr<Node> next;
+  public:
+	Node (T val) : value (val), next (nullptr)
+	{
+	}
+	T value;
+	std::unique_ptr<Node> next;
 };
 
-template <typename T>
-class ForwardList
+template <typename T> class ForwardList
 {
-public:
-  std::unique_ptr<Node<T> > head;
+  public:
+	std::unique_ptr<Node<T> > head;
 
-  ForwardList () : head (nullptr) {}
-  ForwardList (T h) : head (std::make_unique<Node<T> > (h)) {}
+	ForwardList () : head (nullptr)
+	{
+	}
+	ForwardList (T h) : head (std::make_unique<Node<T> > (h))
+	{
+	}
 
-  void push (T val)
-  {
-    if (!head)
-      {
-        head = std::make_unique<Node<T> > (val);
-      }
-    else
-      {
-        Node<T> *temp = head.get ();
-        while (temp->next)
-          {
-            temp = temp->next.get ();
-          }
-        temp->next = std::make_unique<Node<T> > (val);
-      }
-  }
+	void push (T val)
+	{
+		if (! head)
+		{
+			head = std::make_unique<Node<T> > (val);
+		}
+		else
+		{
+			Node<T> *temp = head.get ();
+			while (temp->next)
+			{
+				temp = temp->next.get ();
+			}
+			temp->next = std::make_unique<Node<T> > (val);
+		}
+	}
 
-  void display ()
-  {
-    Node<T> *temp = head.get ();
-    std::cout << "{";
-    while (temp)
-      {
-        std::cout << temp->value;
-        temp = temp->next.get ();
-        if (temp)
-          {
-            std::cout << ", ";
-          }
-      }
-    std::cout << "}\n";
-  }
+	void display ()
+	{
+		Node<T> *temp = head.get ();
+		std::cout << "{";
+		while (temp)
+		{
+			std::cout << temp->value;
+			temp = temp->next.get ();
+			if (temp)
+			{
+				std::cout << ", ";
+			}
+		}
+		std::cout << "}\n";
+	}
 
-  void reverse ()
-  {
-    std::unique_ptr<Node<T> > prev = nullptr;
-    std::unique_ptr<Node<T> > current = std::move (head);
-    while (current)
-      {
-        std::unique_ptr<Node<T> > next = std::move (current->next);
-        current->next = std::move (prev);
-        prev = std::move (current);
-        current = std::move (next);
-      }
-    head = std::move (prev);
-  }
+	void reverse ()
+	{
+		std::unique_ptr<Node<T> > prev = nullptr;
+		std::unique_ptr<Node<T> > current = std::move (head);
+		while (current)
+		{
+			std::unique_ptr<Node<T> > next = std::move (current->next);
+			current->next = std::move (prev);
+			prev = std::move (current);
+			current = std::move (next);
+		}
+		head = std::move (prev);
+	}
 };
 
 int main ()
 {
-  ForwardList<int> list (10);
-  list.push (20);
-  list.push (30);
-  std::cout << "Original list: ";
-  list.display ();
-  list.reverse ();
-  std::cout << "Reversed list: ";
-  list.display ();
+	ForwardList<int> list (10);
+	list.push (20);
+	list.push (30);
+	std::cout << "Original list: ";
+	list.display ();
+	list.reverse ();
+	std::cout << "Reversed list: ";
+	list.display ();
 
-  ForwardList<char> list1 ('a');
-  list1.push ('b');
-  list1.push ('c');
-  std::cout << "Original list: ";
-  list1.display ();
-  list1.reverse ();
-  std::cout << "Reversed list: ";
-  list1.display ();
+	ForwardList<char> list1 ('a');
+	list1.push ('b');
+	list1.push ('c');
+	std::cout << "Original list: ";
+	list1.display ();
+	list1.reverse ();
+	std::cout << "Reversed list: ";
+	list1.display ();
 
-  return 0;
+	return 0;
 }

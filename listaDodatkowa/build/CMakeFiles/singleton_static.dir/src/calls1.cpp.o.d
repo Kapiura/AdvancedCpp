261d51b7f020a63b5489f1024229b559bf52decb
kapiura <swinxten@gmail.com>
2025-01-26 12:46:41 +0100

    gitignore update

--- listaDodatkowa/build/CMakeFiles/singleton_static.dir/src/calls1.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/singleton_static.dir/src/calls1.cpp.o: \
- /home/chania/dev/advCpp/listaDodatkowa/src/calls1.cpp \
- /usr/include/stdc-predef.h \
- /home/chania/dev/advCpp/listaDodatkowa/src/../include/calls1.h \
- /home/chania/dev/advCpp/listaDodatkowa/src/../include/singleton.h
